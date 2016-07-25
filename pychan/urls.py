c6699c4455de5db2475f15a613a4115cc4399691
Jack Evans <jack@evans.gb.net>
2016-07-25 22:05:48 +0100

    PEP8'd

--- pychan/urls.py
+++ pychan/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
