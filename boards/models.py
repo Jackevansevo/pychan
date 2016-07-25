d3b5e7a0d0fad2d9842cfe7db97474c1ba4b1b30
Jack Evans <jack@evans.gb.net>
2016-07-25 22:03:24 +0100

    Added utility methods to Thread count to calculate when a Thread 404's

--- boards/models.py
+++ boards/models.py
@@ -2,7 +2,11 @@ from autoslug import AutoSlugField
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.db import models
+from django.utils.functional import cached_property
 
+# [TODO]
+# Order threads by bump count
+# Prevent submitting images to threads that have 404'd
 # Save User Filters in DB, also have filters tied to a session
 # Implement thread bump count functionality
 # Limit number of POST requests from certain IP Addresses / Sessions
@@ -34,8 +38,9 @@ class Board(models.Model):
     short_name = models.CharField(max_length=3)
     slug = AutoSlugField(populate_from='short_name')
 
+    @cached_property
     def get_absolute_url(self):
-        return reverse('boards:board-detail', args=[self.slug])
+        return reverse('boards:board-detail', kwargs={'slug': self.slug})
 
     def __str__(self):
         return self.name
@@ -48,13 +53,45 @@ class Thread(TimeStampedModel):
     board = models.ForeignKey(Board)
 
     class Meta:
+        # [TODO] Order by bump count
         ordering = ['-created_on']
+
+    @cached_property
+    def bump_count(self):
+        # Returns the bump count of the Thread, placeholder for now
+        pass
+
+    @cached_property
+    def hit_reply_limit(self):
+        # Returns True if Thread has passed it's reply limit
+        replies = Reply.objects.all().filter(thread=self).count()
+        if replies > 5:
+            return True
+        return False
+
+    @cached_property
+    def is_active(self):
+        # Returns True if Thread has had a reply in the last 5 minutes
+        latest_reply = Reply.objects.all().filter(thread=self).last()
+        return latest_reply.created_on
+
+    @cached_property
+    def has_404d(self):
+        # Returns True if Thread has 404'd
+        if self.hit_reply_limit:
+            return True
+        return False
+
+    @cached_property
+    def get_absolute_url(self):
+        return reverse('boards:thread-detail', kwargs={'slug': self.board.slug,
+                                                       'pk': self.id})
 
     def __str__(self):
         return self.title
 
 
-class Reply(models.Model):
+class Reply(TimeStampedModel):
     replies = models.ManyToManyField("self", blank=True)
     user = models.ForeignKey(User, blank=True, null=True)
     thread = models.ForeignKey(Thread)
