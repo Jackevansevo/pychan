735722643d2a77d28871045d5831cac2a51b5cbb
Jack Evans <jack@evans.gb.net>
2016-08-21 20:04:12 +0100

    Make board slug a prepopulated filed, registered Poster Admin

--- boards/admin.py
+++ boards/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
-from .models import Board, Filter, Reply, Thread
+from .models import Board, Reply, Thread, Poster
+
+
+class BoardAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("short_name",)}
+
+
+class PosterAdmin(admin.ModelAdmin):
+    list_display = ('join_date', 'filters', 'karma')
+
 
 # Register your models here.
-admin.site.register(Board)
-admin.site.register(Filter)
+admin.site.register(Board, BoardAdmin)
 admin.site.register(Reply)
 admin.site.register(Thread)
+admin.site.register(Poster, PosterAdmin)
