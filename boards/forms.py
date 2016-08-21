ace2b35a4b0ef7d238868f4a9660295165ffd3bc
Jack Evans <jack@evans.gb.net>
2016-08-21 20:05:04 +0100

    Sorted inputs

--- boards/forms.py
+++ boards/forms.py
@@ -1,9 +1,9 @@
+from django import forms
+
 from .models import Thread, Reply
-from django.core.exceptions import ValidationError
-from django.forms import ModelForm
 
 
-class ThreadCreateForm(ModelForm):
+class ThreadCreateForm(forms.ModelForm):
     class Meta:
         model = Thread
         exclude = ['board', 'bump_count', 'expired']
@@ -12,13 +12,19 @@ class ThreadCreateForm(ModelForm):
         image = self.cleaned_data.get('image', False)
         if image:
             if image._size > 4*1024*1024:
-                raise ValidationError("Image file too large (> 4mb)")
+                raise forms.ValidationError("Image file too large (> 4mb)")
             return image
         else:
-            raise ValidationError("Couldn't read uploaded image")
+            raise forms.ValidationError("Couldn't read uploaded image")
 
 
-class ReplyForm(ModelForm):
+class ReplyForm(forms.ModelForm):
     class Meta:
         model = Reply
         fields = ['content', 'image']
+
+    def clean(self):
+        # [TODO] Do this check in Javascript, no point sending a get request if
+        # no data has been provided
+        if self.cleaned_data['content'] == '' and self.cleaned_data['image'] is None:
+            raise forms.ValidationError("Please provide some data")
