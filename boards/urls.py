387184753a05b47d115daa0e26beacdbd7214e50
Jack Evans <jack@evans.gb.net>
2016-08-21 20:11:11 +0100

    removed redundant Urls

--- boards/urls.py
+++ boards/urls.py
@@ -10,15 +10,9 @@ urlpatterns = [
     url(r'^(?P<slug>[\w-]+)/catalog$', views.BoardDetail.as_view(),
         name='board-detail'),
 
-    url(r'^(?P<slug>[\w-]+)/catalog/create-thread$',
-        views.ThreadCreate.as_view(), name='create-thread'),
-
     url(r'^(?P<slug>[\w-]+)/thread/(?P<pk>[0-9]+)$',
         views.ThreadDetail.as_view(),
         name='thread-detail'),
-
-    url(r'^(?P<slug>[\w-]+)/thread/(?P<pk>[0-9]+)/reply$',
-        views.ReplyCreate.as_view(), name='thread-reply'),
 
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
