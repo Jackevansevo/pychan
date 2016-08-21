7622a8e056bd66be23f672a34e156934da1ab494
Jack Evans <jack@evans.gb.net>
2016-08-21 20:10:57 +0100

    replaced most of style forms with django widget-tweaks

--- boards/templatetags/style-forms.py
+++ boards/templatetags/style-forms.py
@@ -1,15 +1,3 @@
 from django.template import Library
-from django.utils.safestring import mark_safe
 
 register = Library()
-
-
-@register.filter(name='addClass')
-def addClass(value, newClass):
-    try:
-        current_classes = value.field.widget.attrs["class"]
-        new_classes = current_classes + " " + newClass
-        value.field.widget.attrs["class"] = mark_safe(new_classes)
-    except KeyError:
-        value.field.widget.attrs["class"] = newClass
-    return value
