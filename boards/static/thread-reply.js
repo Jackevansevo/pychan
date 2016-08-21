11aea6afc05d1edb2619f32f79341d55290e34e3
Jack Evans <jack@evans.gb.net>
2016-08-21 20:11:47 +0100

    JS code for thread-reply page, scrolling / validation

---
+++ boards/static/thread-reply.js
@@ -0,0 +1,35 @@
+// Show error message if both content and image fields are blank on form submit
+$('#reply-form').submit(function( event ) {
+    if ($('#id_content').val() == '' && $('#id_image').val() == '') {
+        $('#validation-error').show();
+        $('#id_content').addClass('is-danger')
+        // Prevent the form submission
+        return false;
+    }
+});
+
+// Closes alerts on button click
+$('.notification > button').click(function(event) {
+    $(this).parent().hide();
+});
+
+// Hide the scroll-to-reply button if current scroll position is at bottom of
+// the viewport
+$(window).scroll(function() {
+    distanceFromBottom = Math.floor($(document).height() - $(document).scrollTop() - $(window).height());
+    if (distanceFromBottom < 300 ) {
+        console.log("memes");
+        $('a.scroll-to-reply').fadeOut('slow');
+    } else {
+        $('a.scroll-to-reply').fadeIn('slow');
+    }
+});
+
+
+// Scrolls to the reply form on button click
+$(".scroll-to-reply").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#reply-form").offset().top
+    }, 800);
+    $('#id_content').focus();
+});
