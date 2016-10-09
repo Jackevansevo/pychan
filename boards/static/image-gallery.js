52573e86dacb3f10995fc7cf428fe97ef4f3aae5
Jack Evans <jack@evans.gb.net>
2016-10-09 19:49:10 +0100

    Updated code to match Airbnb JS styleguide

--- boards/static/image-gallery.js
+++ boards/static/image-gallery.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
 
     // Get a list of images and their image-preview
-    const modals = Array.from(document.getElementsByClassName("image-preview")).map(el => el.getAttribute('data-target'));
+    var previewModals = Array.from(document.getElementsByClassName('image-preview'));
+    const modals = previewModals.map(el => el.getAttribute('data-target'));
 
     // Set the Max index
-    max_index = modals.length -1;
+    var maxIndex = modals.length -1;
 
     var index;
 
@@ -13,15 +14,15 @@ $(document).ready(function() {
         // A key press should remove the previously active Modal
         $(modals[index]).removeClass('is-active');
 
-        if (typeof index == 'undefined')  index = 0; 
+        if (typeof index === 'undefined')  index = 0;
 
         // Handle the key press
         switch(key.which) {
             case 37:
-                if(index == 0) index = max_index + 1; index -= 1;
+                if(index === 0) index = maxIndex + 1; index -= 1;
                 break;
             case 39:
-                if(index == max_index) index = -1; index += 1;
+                if(index === maxIndex) index = -1; index += 1;
                 break;
             default:
                 return;
@@ -33,14 +34,14 @@ $(document).ready(function() {
     });
 
     // Remove the Model is-active class if the close button is clicked
-    $(".modal-close").click(function() {
+    $('.modal-close').click(function() {
         $(modals[index]).removeClass('is-active');
-    })
+    });
 
     // Clicking the image will display the Modal
-    $(".image-preview").click(function() {
-        modal = $(this).attr('data-target');
-        index = modals.indexOf(modal)
+    $('.image-preview').click(function() {
+        var modal = $(this).attr('data-target');
+        index = modals.indexOf(modal);
         $(modal).addClass('is-active');
     });
 
