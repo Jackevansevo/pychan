3b189e3e9388da49882f44e0cfced4045b353118
Jack Evans <jack@evans.gb.net>
2016-10-09 13:00:38 +0100

    Alter urls to reflect new views

--- boards/urls.py
+++ boards/urls.py
@@ -5,15 +5,17 @@ from . import views
 
 app_name = "boards"
 urlpatterns = [
-    url(r'^$', views.Index.as_view(), name='index'),
-
-    url(r'^(?P<slug>[\w-]+)/catalog$', views.BoardDetail.as_view(),
-        name='board-detail'),
-
-    url(r'^(?P<slug>[\w-]+)/thread/(?P<pk>[0-9]+)$',
-        views.ThreadDetail.as_view(),
-        name='thread-detail'),
-
+    url(r'^$', views.index, name='index'),
+    url(
+        r'^(?P<slug>[\w-]+)/catalog/$',
+        views.board_detail,
+        name='board-detail'
+    ),
+    url(
+        r'^(?P<slug>[\w-]+)/thread/(?P<pk>[0-9]+)/$',
+        views.thread_view,
+        name='thread-detail'
+    ),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
 ]
