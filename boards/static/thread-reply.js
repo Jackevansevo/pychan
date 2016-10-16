0649a2a6d57a6dc984d6b83135681cc09d29ff30
Jack Evans <jack@evans.gb.net>
2016-10-16 19:11:25 +0100

    Fixed javascript style inconsistencies

--- boards/static/thread-reply.js
+++ boards/static/thread-reply.js
@@ -1,22 +1,24 @@
 // Show error message if both content and image fields are blank on form submit
-$('#reply-form').submit(function( event ) {
-    if ($('#id_content').val() == '' && $('#id_image').val() == '') {
+$('#reply-form').submit(function() {
+    if ($('#id_content').val() === '' && $('#id_image').val() === '') {
         $('#validation-error').show();
-        $('#id_content').addClass('is-danger')
+        $('#id_content').addClass('is-danger');
         // Prevent the form submission
         return false;
     }
 });
 
 // Closes alerts on button click
-$('.notification > button').click(function(event) {
+$('.notification > button').click(function() {
     $(this).parent().hide();
 });
 
 // Hide the scroll-to-reply button if current scroll position is at bottom of
 // the viewport
 $(window).scroll(function() {
-    distanceFromBottom = Math.floor($(document).height() - $(document).scrollTop() - $(window).height());
+    var distanceFromBottom = Math.floor(
+        $(document).height() - $(document).scrollTop() - $(window).height()
+    );
     if (distanceFromBottom < 300 ) {
         $('a.scroll-to-reply').fadeOut('slow');
     } else {
@@ -26,9 +28,9 @@ $(window).scroll(function() {
 
 
 // Scrolls to the reply form on button click
-$(".scroll-to-reply").click(function() {
+$('.scroll-to-reply').click(function() {
     $('html, body').animate({
-        scrollTop: $("#reply-form").offset().top
+        scrollTop: $('#reply-form').offset().top
     }, 800);
     $('#id_content').focus();
 });
