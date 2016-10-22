c03de6619169be46682e08acd00c619b27d87c23
Jack Evans <jack@evans.gb.net>
2016-10-22 22:15:47 +0100

    idk tbh

--- boards/static/thread-reply.js
+++ boards/static/thread-reply.js
@@ -10,27 +10,6 @@ $('#reply-form').submit(function() {
 
 // Closes alerts on button click
 $('.notification > button').click(function() {
+        $('#id_content').removeClass('is-danger');
     $(this).parent().hide();
-});
-
-// Hide the scroll-to-reply button if current scroll position is at bottom of
-// the viewport
-$(window).scroll(function() {
-    var distanceFromBottom = Math.floor(
-        $(document).height() - $(document).scrollTop() - $(window).height()
-    );
-    if (distanceFromBottom < 300 ) {
-        $('a.scroll-to-reply').fadeOut('slow');
-    } else {
-        $('a.scroll-to-reply').fadeIn('slow');
-    }
-});
-
-
-// Scrolls to the reply form on button click
-$('#scroll-to-reply').click(function() {
-    $('html, body').animate({
-        scrollTop: $('#reply-form').offset().top
-    }, 800);
-    $('#id_content').focus();
 });
